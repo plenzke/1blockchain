ad0ad9ecfb923d64081e6a48c61569154f41e30a
plenzke <3njoyth3sil3nc399@gmail.com>
2021-03-09 14:27:02 +0900

    added contract

---
+++ 01_blockchain.sol
@@ -0,0 +1,63 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Test
+{
+    enum RequestType {NewHome, EditHome}
+    
+    struct Ownership
+    {
+        string homeAddress;
+        address owner;
+        uint p;
+    }
+    
+    struct Owner
+    {
+        string name;
+        uint passSer;
+        uint passNum;
+        string date; //TODO переделать
+        string phoneNumber;
+    }
+    
+    struct Home
+    {
+        string homeAddress;
+        uint area;
+        uint cost;
+    }
+    
+    struct Request
+    {
+        RequestType requestType;
+        Home home;
+        uint result;
+    }
+    
+    struct Employee
+    {
+        string name;
+        string position;
+        string phoneNumber;
+    }
+    
+    mapping(address => Employee) private emplyees;
+    mapping(address => Owner) private owners;
+    mapping(address => Request) private requests;
+    mapping(string => Home) private homes;
+    mapping(string => Ownership[]) private ownerships;
+    
+    function AddHome(string memory _adr, uint _area, uint _cost) public
+    {
+        Home memory h;
+        h.homeAddress = _adr;
+        h.area = _area;
+        h.cost = _cost;
+        homes[_adr] = h;
+    }
+    
+    function GetHome(string memory adr) public returns(uint _area, uint _cost)
+    {
+        return (homes[adr].area, homes[adr].cost);
+    }
+}
