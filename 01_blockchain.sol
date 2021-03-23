6ba3eaa6ebb5bb894f45deda8b7284552096d54a
plenzke <3njoyth3sil3nc399@gmail.com>
2021-03-23 13:48:49 +0900

    added requests list, delete employee, newhome request

--- 01_blockchain.sol
+++ 01_blockchain.sol
@@ -1,8 +1,43 @@
-pragma solidity >=0.4.22 <0.6.0;
+pragma solidity >=0.7.0 <=0.8.2;
 
-contract Test
+contract Owned
+{
+    address private owner;
+    
+    constructor() 
+    {
+        owner = msg.sender;
+    }
+    
+    modifier OnlyOwner
+    {
+        require(
+            msg.sender == owner);
+        _;
+    }
+    
+    function ChangeOwner(address newOwner) public OnlyOwner
+    {
+        owner = newOwner;
+    }
+    
+    function GetOwner() public returns (address)
+    {
+        return owner;
+    }
+}
+
+
+contract ROSReestr is Owned 
 {
     enum RequestType {NewHome, EditHome}
+    uint reqId;
+    
+    mapping(address => Employee) private employees;
+    mapping(address => Owner) private owners;
+    mapping(uint => Request) private requests;
+    mapping(string => Home) private homes;
+    mapping(string => Ownership[]) private ownerships;
     
     struct Ownership
     {
@@ -10,14 +45,32 @@ contract Test
         address owner;
         uint p;
     }
-    
+
     struct Owner
     {
         string name;
-        uint passSer;
-        uint passNum;
-        string date; //TODO переделать
-        string phoneNumber;
+        uint pass_ser;
+        uint pass_num;
+        uint256 pass_date;
+        string phone_number;
+    }
+    
+    struct Employee
+    {
+        string nameEmpl;
+        string position;
+        string phone_number;
+        bool isset;
+    }
+    
+    struct Request
+    {
+        uint id;
+        RequestType requestType;
+        string homeAddr;
+        uint homeArea;
+        uint homeCost;
+        uint result;
     }
     
     struct Home
@@ -27,27 +80,16 @@ contract Test
         uint cost;
     }
     
-    struct Request
+    
+    modifier OnlyEmployee
     {
-        RequestType requestType;
-        Home home;
-        uint result;
+        require(
+            employees[msg.sender].isset != false
+            );
+            _;
     }
     
-    struct Employee
-    {
-        string name;
-        string position;
-        string phoneNumber;
-    }
-    
-    mapping(address => Employee) private emplyees;
-    mapping(address => Owner) private owners;
-    mapping(address => Request) private requests;
-    mapping(string => Home) private homes;
-    mapping(string => Ownership[]) private ownerships;
-    
-    function AddHome(string memory _adr, uint _area, uint _cost) public
+    function AddHome(string memory _adr, uint _area, uint _cost) public 
     {
         Home memory h;
         h.homeAddress = _adr;
@@ -56,8 +98,56 @@ contract Test
         homes[_adr] = h;
     }
     
-    function GetHome(string memory adr) public returns(uint _area, uint _cost)
+    function GetHome(string memory adr) public returns (uint _area, uint _cost)
     {
         return (homes[adr].area, homes[adr].cost);
+    }
+    
+    function AddEmployee(address id, string memory _name, string memory _position, string memory _phoneNumber) public OnlyOwner
+    {
+        Employee memory emp;
+        emp.nameEmpl = _name;
+        emp.position = _position;
+        emp.phone_number = _phoneNumber;
+        emp.isset = true;
+        employees[id] = emp;
+    }
+    
+    function GetEmployee(address id) public OnlyEmployee OnlyOwner returns (string memory _name, string memory _position, string memory _phoneNumber)
+    {
+        return (employees[id].nameEmpl, employees[id].position, employees[id].phone_number);
+    }
+    
+    function EditEmployee(address _id, string memory _newname, string memory _newposition, string memory _newphoneNumber) public OnlyEmployee OnlyOwner
+    {
+        employees[_id].nameEmpl = _newname;
+        employees[_id].position = _newposition;
+        employees[_id].phone_number = _newphoneNumber;
+    }
+   
+    function DeleteEmployee(address id) public OnlyOwner
+    {
+        delete employees[id];
+    }
+    
+    function AddNewHomeRequest(string memory _homeAddress, uint _homeArea, uint _homeCost) public
+    {
+        Request memory req;
+        req.id = reqId;
+        req.requestType = RequestType.NewHome;
+        req.homeAddr = _homeAddress;
+        req.homeArea = _homeArea;
+        req.homeCost = _homeCost;
+        requests[reqId] = req;
+        reqId++;
+    }
+    
+    function GetRequestsList() public OnlyEmployee returns (Request[] memory request)
+    {
+        request = new Request[](reqId);
+        for (uint _i = 0; _i < reqId; _i++){
+            request[_i] = requests[_i];
+        }
+        return request;
     }
 }
